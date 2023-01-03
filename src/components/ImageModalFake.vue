7e8da7640378f7d01bcc12bb04b6da042ab1daba
Paladiin <92659828+Paladiin@users.noreply.github.com>
2023-01-03 16:55:03 +0000

    Logging changes

---
+++ src/components/ImageModalFake.vue
@@ -0,0 +1,22 @@
+<template>
+  <div :class="{visible: visible, modal: true, imgHeightGtWindow: imgHeightGtWindow}" @click="hide">
+    <img :src="src" class="modal-image">
+  </div>
+</template>
+
+<script>
+export default {
+  props: ['src', 'visible'],
+  data () {
+    return {
+      imgHeightGtWindow: false
+    }
+  },
+  methods: {
+    hide (e) {
+      this.$emit('hide')
+    }
+  },
+  mounted () {
+    const _this = this
+  
