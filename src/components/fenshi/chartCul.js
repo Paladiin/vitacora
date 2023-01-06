f341067d395ea6c50ffb5db25ab5676ebe55cae2
Paladiin <92659828+Paladiin@users.noreply.github.com>
2023-01-06 08:12:50 +0000

    USB: storage: Add quirk for Adaptec USBConnect 2000 USB-to-SCSI Adapter

--- src/components/fenshi/chartCul.js
+++ src/components/fenshi/chartCul.js
@@ -50,4 +50,84 @@ var cul = {
       var diff = data_set1.map(function(d1, index){
         var data2 = data_set2[index];
 
-        var result =
+        var result = d1.slice();
+        val_indexes.forEach(function(val_index){
+          var diff = data2[val_index] - result[val_index];
+          result[val_index] = diff / 60;
+        });
+        return result;
+      });
+
+      return function(){
+        data_set1.forEach(function(d1, index){
+          val_indexes.forEach(function(val_index){
+            d1[val_index] += diff[index][val_index];
+          });
+        });
+      }
+    }
+  },
+
+  Chart: {
+    linkCharts: function(charts){
+      charts.forEach(function(chart){
+        charts.forEach(function(other_chart){
+          if (chart !== other_chart)
+            chart.linked_charts.insert(other_chart);
+        });
+      });
+    }
+  },
+  DataTypes: {
+    Set: function(){
+      var set = function(d){
+        this.d = d || [];
+      };
+      set.prototype.insert = function(item){
+        if (this.d.indexOf(item) < 0)
+          this.d.push(item);
+      };
+      set.prototype.remove = function(item){
+        var index = this.d.indexOf(item);
+        if (index > -1)
+          this.d.splice(index, 1);
+      };
+      set.prototype.length = function(){
+        return this.d.length;
+      };
+      set.prototype.forEach = function(func){
+        this.d.forEach(func);
+      };
+
+      return function(lst){
+        return new set(lst);
+      };
+    }()
+  },
+  Math: {
+    sum: function(lst){
+      var sum = 0;
+      lst.forEach(function(item){
+        sum += item;
+      });
+      return sum;
+    },
+    // get Standard Deviation
+    getSD: function(data, avg){
+      if (avg === undefined){
+        avg = cul.Math.sum(data) / data.length;
+      }
+      return Math.sqrt(
+              cul.Math.sum(
+                data.map(function(item){return Math.pow(item - avg, 2)}))
+              / data.length);
+    },
+    iterOffsetN: function(data, index, n, callback){
+      if (!n){
+        return;
+      }
+
+      var target = index + (Math.abs(n) - 1) * (n > 0 ? 1 : -1);
+      if (target < 0)
+        target = 0;
+      
