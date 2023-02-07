1f961fb4290f730c30aa99f1a6c7c50dc50a8879
Paladiin <92659828+Paladiin@users.noreply.github.com>
2023-02-07 22:38:19 +0000

    1.7.7

---
+++ src/components/plateItem/plateItem.vue
@@ -0,0 +1,9 @@
+<template>
+<div >
+    <div class="plate-item" @click="_redirect">
+        <dl>
+            <dt>
+                <div class="title">
+                    <span :class="index>2?'gray':''">{{index + 1}}</span>
+                    <h3>{{info.title}}</h3>
+                </di
